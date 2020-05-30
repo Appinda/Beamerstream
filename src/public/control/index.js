d6ad9e1b4691b957cd1e2156cb61664ee3541ce7
maartenverheul <maarten.verheul@student.hu.nl>
2020-05-30 20:51:20 +0200

    Small optimizations

--- src/public/control/index.js
+++ src/public/control/index.js
@@ -14,31 +14,13 @@ ipcRenderer.send('getSongs')
 //                FUNCTIONS
 // ===========================================
 
-function up() {
-  let active = $(".outputlist > .active");
-  let prev = $(".outputlist > .active").prev();
-  if (prev.length == 0) return false;
-  prev.addClass("active");
-  active.removeClass("active");
-  setCurrentText();
-  return true;
-}
-function down() {
-  let active = $(".outputlist > .active");
-  let next = $(".outputlist > .active").next();
-  if (next.length == 0) return false;
-  next.addClass("active");
-  active.removeClass("active");
-  setCurrentText();
-  return true;
-}
-
 function decodeLyrics(text) {
   let rawtext = text;
   rawtext = rawtext.replace(/\n/g, "<br/>");
   return rawtext;
 }
 function encodeLyrics(rawtext) {
+  if(rawtext == null) throw "Parameter error";
   let text = rawtext;
   text = text.replace(/<\/?br\/?>/g, "\n");
   return text;
@@ -46,7 +28,7 @@ function encodeLyrics(rawtext) {
 
 function setCurrentText() {
   let html = $(".outputlist > .active").html();
-  setText(html);
+  if(html) setText(html);
 }
 function setText(text) {
   ipcRenderer.send('setText', { text: encodeLyrics(text) })
@@ -56,7 +38,7 @@ function loadSonglist(songlist){
   clearSonglist();
   for (let songid in songlist) {
     let song = songlist[songid];
-    $(".songlist").append(`<div data-id="${song.id}">${song.name} <small>- ${song.author}</small></div>`);
+    $(".songlist").append(`<div class="hover" data-id="${song.id}">${song.name} <small>- ${song.author}</small></div>`);
   }
   listenSonglist();
 }
@@ -87,12 +69,22 @@ function loadSong(song){
     if(!song.lyrics.hasOwnProperty(versename)) { console.error(`Error while parsing song! Verse order contains verse "${versename}", which has not been specified in lyrics.`); continue; }
     let verse = song.lyrics[versename].replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;").replace(/%n/g, "<br/>").replace(/{sm}/g, "<small>").replace(/{\/sm}/g, "</small>").replace(/{y}/g, "<span class='stl-y'>").replace(/{\/y}/g, "</span>");
     let verses = verse.split("%s");
+    $(".outputlist").append(`<div class="title">${getVerseFullName(versename)}</div>`);
     for(let verse of verses){
-      $(".outputlist").append(`<div>${verse}</div>`);
+      $(".outputlist").append(`<div class="verse hover">${verse}</div>`);
     }
   }
   listenOutputlist();
   return true;
+}
+function getVerseFullName(shortname){
+  if(shortname.startsWith("V")) return shortname.replace('V', 'Verse ')
+  if(shortname.startsWith("C")) return shortname.replace('C', 'Chorus ')
+  if(shortname.startsWith("E")) return shortname.replace('E', 'Ending ')
+  if(shortname.startsWith("O")) return shortname.replace('O', 'Other ')
+  if(shortname.startsWith("B")) return shortname.replace('B', 'Bridge ')
+  if(shortname.startsWith("P")) return shortname.replace('P', 'Pre-Chorus ')
+  return shortname;
 }
 function unloadSong(){
   $(".outputlist").empty();
@@ -101,7 +93,7 @@ function deselectOutput(){
   $(".outputlist > .active").removeClass("active");
 }
 function listenOutputlist(){
-  $(".outputlist > div").click(e => {
+  $(".outputlist > div.hover").click(e => {
     deselectOutput();
     $(e.target).addClass("active");
     setCurrentText();
@@ -109,13 +101,13 @@ function listenOutputlist(){
 }
 function listenSonglist(){
   // SOCKETLIST CLICK
-  $(".songlist > div").click(e => {
+  $(".songlist > div.hover").click(e => {
     $(".songlist > .active").removeClass("active");
     $(e.target).addClass("active");
   });
 
   // DOUBLE CLICK SONGLIST
-  $(".songlist > div").dblclick(e => {
+  $(".songlist > div.hover").dblclick(e => {
     let id = $(e.target).attr('data-id');
     loadSongById(id);
     $(".songlist > .active").removeClass("active");
@@ -129,6 +121,30 @@ function setFontSize(size){
 function hide(){
   deselectOutput();
   setText("");
+  return true;
+}
+function up() {
+  let active = $(".outputlist > .active");
+  let prev = active.prev();
+  while(!prev.hasClass("hover")) {
+    prev = prev.prev();
+    if (prev.length == 0) return false;
+  }
+  prev.addClass("active");
+  active.removeClass("active");
+  setCurrentText();
+  return true;
+}
+function down() {
+  let active = $(".outputlist > .active");
+  let next = active.next();
+  while(!next.hasClass("hover")) {
+    next = next.next();
+    if (next.length == 0) return false;
+  }
+  next.addClass("active");
+  active.removeClass("active");
+  setCurrentText();
   return true;
 }
 
@@ -151,8 +167,8 @@ $(() => {
       case "w":
         hide();
         break;
-      default:
-        console.log(e.key);
+      // default:
+        // console.log(e.key);
     }
   });
 
