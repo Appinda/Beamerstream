bdf3356f4c5c4d391c85ecb14706e61615cee60b
maartenverheul <maarten.verheul@student.hu.nl>
2020-06-05 11:32:05 +0200

    Add nuxt project

---
+++ src/server/main.js
@@ -0,0 +1,55 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+// ============================================
+//                   ELECTRON
+// ============================================
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    title: "Beamerstream",
+    webPreferences: {
+      // preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.maximize();
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show();
+  })
+}
+
+function listen(){
+  app.whenReady().then(() => {
+    createWindow()
+    
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  });
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// ============================================
+//                    SERVER
+// ============================================
+
+const express = require('express')
+const server = express()
+const port = 3000
+
+server.use('/', express.static(path.join(__dirname, '../generate')));
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+  listen();
+});
