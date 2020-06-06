c72f7af5916a45ef3b867b779a1090347c95bccb
maartenverheul <maarten.verheul@student.hu.nl>
2020-06-06 14:59:55 +0200

    Fix header path

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,9 +7,9 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/control">Control</b-nav-item>
-          <b-nav-item to="/control/songs">Songs</b-nav-item>
-          <b-nav-item to="/control/settings"><b-icon-gear-fill/></b-nav-item>
+          <b-nav-item to="/client">Control</b-nav-item>
+          <b-nav-item to="/client/songs">Songs</b-nav-item>
+          <b-nav-item to="/client/settings"><b-icon-gear-fill/></b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
