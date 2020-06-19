6f36e425f91a48cad1e06aaf1b737b8f78d6d65e
maartenverheul <maarten.verheul@student.hu.nl>
2020-06-19 14:28:46 +0200

    Add components file

---
+++ src/components/Songlist.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="songlist bs-select">
+    <div
+      v-for="(item, index) in songlist"
+      :key="index"
+      :data-id="item.id"
+      @dblclick="loadSong"
+      @click="selectSong"
+    >
+      {{item.name}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Songlist",
+  props: {
+    songlist: Array
+  },
+  methods: {
+    selectSong(e) {
+      e.preventDefault();
+      console.log("SS", e.target.getAttribute("data-id"));
+    },
+    loadSong(e) {
+      e.preventDefault();
+      console.log("LS", e.target.getAttribute("data-id"));
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.songlist {
+  > div small {
+    color: #888;
+  }
+  .error {
+    color: red;
+  }
+}
+</style>
---
+++ src/plugins/components.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+import Header from '~/components/Header';
+import Songlist from '~/components/Songlist';
+
+Vue.component('bs-header', Header);
+Vue.component('bs-songlist', Songlist);
